2b3c7dafd750355aecd50e0297b58b0c1d4fe7d8
rhehdgus8831 <rhehdgus8831@naver.com>
2025-07-30 10:54:14 +0900

    JPA 기반 회원, 상품, 구매 레포지토리 및 구매 기록 생성 테스트 클래스 추가.

---
+++ src/main/java/com/spring/database/jpa/chap04/repository/GoodsRepository.java
@@ -0,0 +1,8 @@
+package com.spring.database.jpa.chap04.repository;
+
+import com.spring.database.jpa.chap04.entity.Goods;
+import com.spring.database.jpa.chap04.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoodsRepository extends JpaRepository<Goods, Long> {
+}
