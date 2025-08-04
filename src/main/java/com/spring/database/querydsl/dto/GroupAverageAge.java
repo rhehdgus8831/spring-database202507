9d0a1ccd8d50890d7f3a66c7ed360e4766708888
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-04 10:03:23 +0900

    QueryDSL 기반 동적 쿼리 및 서브쿼리 테스트 추가: 아이돌 및 그룹 조회용 동적 쿼리(QueryDslDynamicTest)와 서브쿼리(QueryDslSubqueryTest) 테스트 클래스 작성, Group 평균 나이 조회 기능 구현 및 API(/api/query/idols/avg) 추가. DDL 설정(create -> update) 변경.

--- src/main/java/com/spring/database/querydsl/dto/GroupAverageAge.java
+++ src/main/java/com/spring/database/querydsl/dto/GroupAverageAge.java
@@ -1,33 +1,32 @@
 package com.spring.database.querydsl.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.querydsl.core.Tuple;
-import com.spring.database.querydsl.entity.QGroup;
 import com.spring.database.querydsl.entity.QIdol;
 import lombok.*;
 
-@EqualsAndHashCode
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
+@EqualsAndHashCode
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
-
 // 그룹명과 평균나이를 매핑할 클래스
 public class GroupAverageAge {
 
+    @JsonProperty("name")
     private String groupName;
+    @JsonProperty("age")
     private Double averageAge;
 
     // Tuple을 전달받아서 DTO로 변환하는 생성자
-
     public GroupAverageAge(Tuple tuple) {
         this.groupName = tuple.get(QIdol.idol.group.groupName);
-        Double averageAge = tuple.get(QIdol.idol.age.avg());
-
+        this.averageAge = tuple.get(QIdol.idol.age.avg());
     }
 
-    // 정적 팩토리 메서드 패턴 (이름을 마음대로 설정 가능)
+    // 정적 팩토리 메서드 패턴
     // Tuple을 전달받아서 DTO로 변환하는 메서드
     public static GroupAverageAge from(Tuple tuple) {
         return GroupAverageAge.builder()
--- src/main/java/com/spring/database/querydsl/repository/GroupRepository.java
+++ src/main/java/com/spring/database/querydsl/repository/GroupRepository.java
@@ -3,7 +3,10 @@ package com.spring.database.querydsl.repository;
 import com.spring.database.querydsl.entity.Album;
 import com.spring.database.querydsl.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface GroupRepository extends JpaRepository<Group, Long> {
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long>, GroupRepositoryCustom {
 
 }
